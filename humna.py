b7f920dc31d939392040b11931c7e8f652931ca9
Behzad <behzadkaviyani83@gmail.com>
2025-08-13 01:57:59 +0430

    This is a human class

---
+++ humna.py
@@ -0,0 +1,11 @@
+class human:
+    def __init__(self, first_name: str, last_name: str, age: int):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def talk(text: str):
+        print(f"human: {text}")
+
+    def walk():
+        print("Walking")
