2c5c6172361827b3aae7230f3cbba5dc8a3d34f7
Behzad <behzadkaviyani83@gmail.com>
2025-08-23 23:59:55 +0430

    refact: set better names for parameters feat: add new feature

--- converter.py
+++ converter.py
@@ -7,6 +7,12 @@ def lbs_to_kg(weight_lbs):
 def cm_to_m(length_cm):
     return length_cm / 100
 
+def m_to_cm(length_m):
+    return length_m *100
+
 def mile_to_kilometer(length_mile):
     return length_mile * 1.6
+
+def kilometer_to_mile(length_kilometer):
+    return length_kilometer / 1.6
 
