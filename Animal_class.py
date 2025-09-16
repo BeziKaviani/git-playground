d18069355f02182ffae682b86701727695e24966
Behzad <behzadkaviyani83@gmail.com>
2025-09-16 11:57:49 +0430

    add eat and sleep function to Animal_class.py -resolve #2

--- Animal_class.py
+++ Animal_class.py
@@ -1,4 +1,11 @@
 class Animal:
     def __init__(self, species):
         self.species = species
-        
+    
+    def eat(food):
+        print(f"Eating {food}")
+    
+    def sleep():
+        print("Sleeping")
+
+            
